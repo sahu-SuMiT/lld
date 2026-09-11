4a94ecebcb3ea9f56b0d92e01d19a9564df55c5c
SuMiT <145762361+sahu-SuMiT@users.noreply.github.com>
2026-09-11 21:34:57 +0530

    Update Factory.java

--- patterns/Factory.java
+++ patterns/Factory.java
@@ -1,33 +1,41 @@
-interface Vehicle{
-  void ride();
+interface Notification{
+  void send();
 }
-class Bike implements Vehicle{
-  public void ride(){
-    System.out.println("Bike Ride");
+class SmsNotification implements Notification{
+  public void send(){
+    System.out.println("SMS notify all");
   }
 }
-class Car implements Vehicle{
-  public void ride(){
-    System.out.println("Car Ride");
+class EmailNotification implements Notification{
+  public void send(){
+    System.out.println("Email notify all");
   }
 }
-class VehicleFactory{
-  public Vehicle createVehicle(String type){
-    if(type.equalsIgnoreCase("car")){
-      return new Car();
-    }else if(type.equalsIgnoreCase("bike")){
-      return new Bike();
-    }
-    return null;
+class WhatsappNotification implements Notification{
+  public void send(){
+    System.out.println("Whatsapp notify all");
   }
 }
+
+class NotificationFactory{
+  public Notification createNotification(String type){
+    if(type.equalsIgnoreCase("sms"))return new SmsNotification();
+    else if(type.equalsIgnoreCase("email"))return new EmailNotification();
+    else if(type.equalsIgnoreCase("whatsapp"))return new WhatsappNotification();
+    else return null;
+  }
+}
+
 public class Main{
   public static void main(String[]args){
-    VehicleFactory factory=new VehicleFactory();
-    Vehicle bike=factory.createVehicle("bike");
-    Vehicle car=factory.createVehicle("car");
+    NotificationFactory factory=new NotificationFactory();
+    Notification email=factory.createNotification("email");
+    Notification whatsapp=factory.createNotification("whatsapp");
+    Notification sms=factory.createNotification("sms");
 
-    bike.ride();
-    car.ride();
+    email.send();
+    whatsapp.send();
+    sms.send();
+
   }
 }
